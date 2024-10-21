698e295c71947327b5b12425758c623765d63324
ivadelser <ivandelgaser@gmail.com>
2024-10-21 21:25:18 +0200

    cookies2

--- cookies/carro.php
+++ cookies/carro.php
@@ -1,11 +1,35 @@
 <?php
+    
     $articulos = array(
-        array("id" => 1, "nombre" => "Zapatillas Nike", "precio" => 60),
-        array("id" => 2, "nombre" => "Sudadera Domyos", "precio" => 15),
-        array("id" => 3, "nombre" => "Pala de pádel Vairo", "precio" => 50),
-        array("id" => 4, "nombre" => "Pelota de baloncesto Molten", "precio"
-        => 20)
-        );
-    $precio_total = 0;
-    echo "Precio Total: ". $precio_total;
+        array("id"=>1, "nombre"=>"Zapatillas Nike", "precio"=>100),
+        array("id"=>2, "nombre"=>"Sudadera Adidas", "precio"=>36),
+        array("id"=>3, "nombre"=>"Pala padel Head", "precio"=>110),
+        array("id"=>4, "nombre"=>"Bolso Dolce & Gabbana", "precio"=>145),
+    );
+
+    session_start();
+    if (isset($_GET['id'])){
+        $idArticulo = $_GET['id'];
+        foreach ($articulos as $articulo){
+            if ($articulo['id'] == $idArticulo){
+                $_SESSION['Carrito'] []= $articulo;
+                break;
+            }
+        }
+    }
+    $totalEuros = 0;
+
+    if (isset($_SESSION['Carrito'])){
+        foreach ($_SESSION['Carrito'] as $precioArt){
+            $totalEuros += $precioArt['precio'];
+        }
+    }
+
+    if (isset($_GET['vaciar']) && $_GET['vaciar'] == 'true') {
+        unset($_SESSION['Carrito']);  
+        session_destroy();
+        $totalEuros = 0;
+    }
+    
+    include('carro.view.php')
 ?>
--- cookies/carro.view.php
+++ cookies/carro.view.php
@@ -3,23 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carro</title>
-    <style>
-
-    </style>
+    <title>Carrito</title>
 </head>
 <body>
-    <?php include "carro.php";?>
-    <ul>
-        <?php foreach($articulos as $articulo): ?>
-            <li>
-                <form method="post">
-                    <button type="submit" name="articulo" value="<?php $articulo["id"];?>">
-                        <?php echo $articulo["nombre"];?> - <?php echo $articulo["precio"];?>
-                    </button>
-                </form>
-            </li>
-        <?php endforeach;?>
-    </ul>
+    <h1>Articulos</h1>
+    <?php
+    echo "<table border=1px>";
+    echo "<tr>";
+        echo "<th>Nombre</th>";
+        echo "<th>Precio</th>";
+        echo "<th>Carrito</th>";
+        echo "</tr>";
+
+        foreach ($articulos as $articulo){
+            echo "<tr>";
+            echo "<td>" . $articulo['nombre'] . "</td>";
+            echo "<td>" . $articulo['precio'] . "</td>";
+            echo "<td><a href='carro.php?id=" . $articulo['id'] . "'>Añadir al carrito</a></td>";
+            echo "</tr>";
+        }
+    echo "</table>";
+    ?>
+    <h2>Carrito Compra</h2>
+    <?php 
+        if (isset($_SESSION['Carrito']) && count($_SESSION['Carrito']) > 0){
+            echo "<table border=1px>";
+            echo "<tr>";
+                echo "<th>Nombre</th>";
+                echo "<th>Precio</th>";
+                echo "</tr>";
+                foreach ($_SESSION['Carrito'] as $item){
+                    echo "<tr>";
+                    echo "<td>" . $item['nombre'] . "</td>";
+                    echo "<td>" . $item['precio'] . "</td>";
+                    echo "</tr>";
+                }
+            echo "</table>";
+            echo "<a href='carro.php?vaciar=true'>Volver</a>";
+        }
+
+        echo "<h3>Total:$totalEuros</h3>";
+
+    ?>
+    
 </body>
 </html>
