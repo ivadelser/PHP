e0d79bf1bbb4bbb0252cce9567e6d5b73259b0dd
ivadelser <ivandelgaser@gmail.com>
2024-12-09 21:30:58 +0100

    Burger1.1

--- burger/View/listar.php
+++ burger/View/listar.php
@@ -8,6 +8,7 @@
         <th>Precio</th>
         <th>Imagen</th>
         <th>Categoría</th>
+        <th>Acciones</th>
     </tr>
     <?php foreach ($burger as $tableBurger): ?>
         <tr>
@@ -15,7 +16,7 @@
             <td><?= $tableBurger["name"] ?></td>
             <td><?= $tableBurger["description"] ?></td>
             <td><?= $tableBurger["price"] ?></td>
-            <td><img src="Imagenes/<?= $tableBurger["image"] ?>" style="width: 100px" ></style></td>
+            <td><img src="Imagenes/<?= $tableBurger["image"] ?>" style="width: 150px" ></style></td>
             <td><?= $tableBurger["category"] ?></td>
             <td><a href="index.php?action=edit&id=<?= $tableBurger['id']?>">Editar</a>
             <a href="index.php?action=delete&id=<?= $tableBurger['id'] ?>" onclick="return confirm('Estás seguro?')">Eliminar</a>
